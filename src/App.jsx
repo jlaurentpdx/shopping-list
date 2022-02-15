b58bd54a2369cda16c714de19b3e80d6279f6db6
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-14 16:36:52 -0800

    created initial ux

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
 export default function App() {
-  return <h1>Hello World</h1>;
+  const initialItems = [
+    { id: 0, item: 'meat' },
+    { id: 1, item: 'potatoes' },
+    { id: 2, item: 'broccoli' },
+  ];
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <form>
+        <input type="text" placeholder="new item" aria-label="new-item" />
+        <button aria-label="submit">Add Item</button>
+      </form>
+      <ul>
+        {initialItems.map((item) => (
+          <li>
+            <h2 style={{ display: 'inline' }}>{item.item}</h2>
+            <button aria-label="edit">Edit</button>
+            <button aria-label="delete">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
