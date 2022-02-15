f65282130352db949c87cb6b10c2934a691c094c
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-14 17:49:11 -0800

    initial styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import styles from './App.css';
+
 export default function App() {
   const initialItems = [
     { id: 0, item: 'meat' },
@@ -5,21 +7,23 @@ export default function App() {
     { id: 2, item: 'broccoli' },
   ];
   return (
-    <div>
+    <div className={styles.App}>
       <h1>Shopping List</h1>
-      <form>
-        <input type="text" placeholder="new item" aria-label="new-item" />
-        <button aria-label="submit">Add Item</button>
-      </form>
-      <ul>
-        {initialItems.map((item) => (
-          <li>
-            <h2 style={{ display: 'inline' }}>{item.item}</h2>
-            <button aria-label="edit">Edit</button>
-            <button aria-label="delete">Delete</button>
-          </li>
-        ))}
-      </ul>
+      <main>
+        <form className={styles.form}>
+          <input type="text" placeholder="new item" aria-label="new-item" />
+          <button aria-label="submit">Add Item</button>
+        </form>
+        <ul className={styles.list}>
+          {initialItems.map((item) => (
+            <li key={item.id} className={styles.listItem}>
+              <h2>{item.item}</h2>
+              <button aria-label="edit">Edit</button>
+              <button aria-label="delete">Delete</button>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
